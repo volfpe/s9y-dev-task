5825ce7119ee115aaf7b70d4fbf84f8ca34b918a
Petr Volf <petr.volf96@gmail.com>
2019-04-09 11:45:00 +0200

    apollo fix

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,16 @@ import withApollo from '../lib/with-apollo'
 import { ApolloProvider } from 'react-apollo'
 
 class MyApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
   render () {
     const { Component, pageProps, apolloClient } = this.props
     return (
